150da40b9196675bd121c5036615820b556e95cf
shet1990 <shet1990@gmail.com>
2018-09-30 22:19:12 +0300

    advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let sumZero = 0;
+    let countZero;
+    let stack = [];
+    let binString = '';
+
+    while (number > 0) {
+        stack.push(number % base);
+        number = Math.floor(number / base);
+    }
+    while (stack.length) {
+        binString = binString + stack.pop();
+    }
+
+    for (n = 1; countZero != 0; n++) {
+        countZero = Math.floor(number / Math.pow(5, n));
+        sumZero = sumZero + countZero;
+    }
+    return sumZero;
 }
