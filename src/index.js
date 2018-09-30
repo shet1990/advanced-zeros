77f7d21d1e5ae62373cc062bb2360c4f25ab78e1
shet1990 <shet1990@gmail.com>
2018-09-30 23:13:51 +0300

    advanced-zeros/rectified

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-    let sumZero = 0;
-    let countZero;
-    let stack = [];
-    let binString = '';
+    let firstParseNumber = number;
+    for (let i = 2; i <= base; i++) {
+        let a = 0;
+        while (base % i == 0) {
+            a++;
+            base = ~~(base / i);
+        }
+        let b = 0;
+        let secondParseNumber = number;
+        while (secondParseNumber / i > 0) {
+            b += ~~(secondParseNumber / i);
+            secondParseNumber = ~~(secondParseNumber / i);
+        }
+        if (firstParseNumber > b / a) {
+            firstParseNumber = b / a;
+        }
 
-    while (number > 0) {
-        stack.push(number % base);
-        number = Math.floor(number / base);
     }
-    while (stack.length) {
-        binString = binString + stack.pop();
-    }
-
-    for (n = 1; countZero != 0; n++) {
-        countZero = Math.floor(number / Math.pow(5, n));
-        sumZero = sumZero + countZero;
-    }
-    return sumZero;
+    return ~~(firstParseNumber);
 }
